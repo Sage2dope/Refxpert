ad86f9465dd57c6888fcec56c8f91ace79b6a2fd
sage2dope <popezamani@gmail.com>
2023-12-09 15:56:34 +0000

    added domain

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-aag1q^^n1enk#7e%heeqwbg_%**si_^&+%6jh!d(ld3qd#g(+h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['refxpert-6397a715f1a3.herokuapp.com', '127.0.0.1', "main.d30t87n9o61xbr.amplifyapp.com"]
+ALLOWED_HOSTS = ['refxpert-6397a715f1a3.herokuapp.com', '127.0.0.1', "www.refxpert.com", "refxpert.com"]
 
 
 # Application definition
